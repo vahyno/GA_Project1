f35b43f56c2ffc469b80e3c66f7eb4e594f6adaf
vahyno <vahyno@gmail.com>
2018-04-02 21:18:40 -0700

    server setup

---
+++ server.js
@@ -0,0 +1,38 @@
+//requirements
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+
+//var db = require('./models');
+//var controllers = require('./controllers');
+
+//middleware
+
+//serve static files
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+//routes
+app.get('/', function (req, res){
+  //res.send('hello world to 1st project API')
+  res.sendFile('views/index.html', {root: __dirname});
+});
+
+app.get('/api/profile', function(req, res){
+  res.json({
+    name:"Michal & Marc",
+    githubUserNames: ["markfdsouza","vahyno"],
+    githubLinks: ["https://github.com/markfdsouza","https://github.com/vahyno"],
+  })
+});
+
+
+
+
+
+// server start
+
+app.listen(3000, function(){
+  console.log('HTTP server listening at localhost:3000');
+});
