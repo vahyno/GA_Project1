e7d82122126469bb759c02b59be3611c3446729f
vahyno <vahyno@gmail.com>
2018-04-04 09:48:36 -0700

    hardcoded version populates indiviadual marker position on each map

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -17,34 +17,34 @@ $(document).ready(function() {
 
 
 
-function initMaps(indx) {
-  var map = new google.maps.Map(document.getElementById(`map-${indx}`), {
-    zoom: 14,
-    center: {lat: 37.78, lng: -122.44}
-  });
-  var geocoder = new google.maps.Geocoder();
-
-  document.getElementById('submitButton').addEventListener('click', function(event) {
-    event.preventDefault();
-    geocodeAddress(geocoder, map);
-  });
-}
-
-function geocodeAddress(geocoder, resultsMap) {
-  var address = document.getElementById('zipcode').value;
-  console.log(address);
-  geocoder.geocode({'address': address}, function(results, status) {
-    if (status === 'OK') {
-      resultsMap.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-        map: resultsMap,
-        position: results[0].geometry.location
-      });
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
-}
+// function initMaps(indx) {
+//   var map = new google.maps.Map(document.getElementById(`map-${indx}`), {
+//     zoom: 14,
+//     center: {lat: 37.78, lng: -122.44}
+//   });
+//   var geocoder = new google.maps.Geocoder();
+//
+//   document.getElementById('submitButton').addEventListener('click', function(event) {
+//     event.preventDefault();
+//     geocodeAddress(geocoder, map);
+//   });
+// }
+//
+// function geocodeAddress(geocoder, resultsMap) {
+//   var address = document.getElementById('zipcode').value;
+//   console.log(address);
+//   geocoder.geocode({'address': address}, function(results, status) {
+//     if (status === 'OK') {
+//       resultsMap.setCenter(results[0].geometry.location);
+//       var marker = new google.maps.Marker({
+//         map: resultsMap,
+//         position: results[0].geometry.location
+//       });
+//     } else {
+//       alert('Geocode was not successful for the following reason: ' + status);
+//     }
+//   });
+// }
 
 var test_people = [{
   id: 123,
@@ -64,3 +64,52 @@ var test_people = [{
   yearOfBirth: 1970,
   zipcode: '255 bush street'
 }];
+
+
+function handleSuccess(people) {
+    people.forEach(function(person) {
+      renderPerson(person);
+    });
+};
+
+function handleError(err){
+  console.log('There has been an error: ', err);
+}
+
+function renderPerson(mapPerson){
+  console.log('rendering individual', mapPerson);
+  var age = new Date().getFullYear() - parseInt(mapPerson.yearOfBirth);
+
+  $('#map-container').append(`
+      <div id="map-${mapPerson.id}" style="height:50%;width:50%;"></div
+
+      <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">${mapPerson.name}</h5>
+          <p class="card-text"> card</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+      <hr>
+    `);
+
+    let address = mapPerson.zipcode;
+    var map = new google.maps.Map(document.getElementById(`map-${mapPerson.id}`),
+    {
+      zoom: 14,
+      center: {lat: 37.78, lng: -122.44}
+    });
+    var geocoder = new google.maps.Geocoder();
+    console.log(address);
+    geocoder.geocode({'address': address}, function(results, status) {
+      if (status === 'OK') {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+        });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
