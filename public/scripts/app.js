432878628de1d96d8aee537eeb4eaf6af0fd329c
vahyno <vahyno@gmail.com>
2018-04-03 17:46:33 -0700

    started moving map to app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,21 +3,35 @@ console.log('sanity check')// wait for the DOM to finish loading
 $(document).ready(function() {
   console.log('document ready');
   // all code to manipulate the DOM goes inside this function
+    var container=$('#map-container');
+    container.append(`
+      <div id="map-1" style="height:50%;width:50%;"></div
 
-    function initMap() {
-      var map = new google.maps.Map(document.getElementById('map'), {
+
+
+
+
+
+
+    `);
+
+
+    function initMaps() {
+      var map = new google.maps.Map(document.getElementById('map-1'), {
         zoom: 14,
         center: {lat: 37.78, lng: -122.44}
       });
       var geocoder = new google.maps.Geocoder();
 
-      document.getElementById('submit').addEventListener('click', function() {
+      document.getElementById('submitButton').addEventListener('click', function(event) {
+        event.preventDefault();
         geocodeAddress(geocoder, map);
       });
     }
 
     function geocodeAddress(geocoder, resultsMap) {
-      var address = document.getElementById('address').value;
+      var address = document.getElementById('zipcode').value;
+      console.log(address);
       geocoder.geocode({'address': address}, function(results, status) {
         if (status === 'OK') {
           resultsMap.setCenter(results[0].geometry.location);
@@ -30,8 +44,7 @@ $(document).ready(function() {
         }
       });
     }
-
-    initMap();
+    initMaps();
 
 
 
