7f47690bc9c28e4bc0ca449421061738f4a23167
vahyno <vahyno@gmail.com>
2018-04-04 12:37:49 -0700

    created test schema-model

--- models/people.js
+++ models/people.js
@@ -2,5 +2,12 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var PeopleSchema = new Schema({
-  
-})
+  id: number,
+  name: String,
+  yearOfBirth: Number,
+  zipcode: String
+});
+
+var Person = mongoose.model('Person', PeopleSchema );
+
+model.exports = Person;
