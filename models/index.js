03476ef4bd87d9c4eca4e1854c9e67596d023ada
vahyno <vahyno@gmail.com>
2019-03-18 20:34:53 -0700

    fixed security vulnerabilities and depricated code

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/project1_map' );
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/project1_map', { useNewUrlParser: true });
 
 
 
